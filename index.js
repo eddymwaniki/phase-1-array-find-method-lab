f13a660f23a3bd7012a73093a9fa7900d0be3530
eddy <eddy.mwaniki.27@gmail.com>
2024-12-09 00:16:03 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(superbowlRecord) {
+    const winningSeason = superbowlRecord.find(superbowlRecord => superbowlRecord.result === "W");
+    return winningSeason ? winningSeason.year : undefined ;
+}
